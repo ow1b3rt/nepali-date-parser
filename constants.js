1b2fc204a080e0038ca9959013164fd1be66e7a4
ow1b3rt <ow1b3rt>
2026-07-06 22:16:25 +0545

    defining possible month names, mapping nepali digits/month names to english digits

---
+++ constants.js
@@ -0,0 +1,98 @@
+export const ENGLISH_MONTHS = {
+    baisakh: 1,
+    baishakh: 1,
+
+    jestha: 2,
+    jyeshtha: 2,
+
+    ashadh: 3,
+    asar: 3,
+
+    shrawan: 4,
+    shravan: 4,
+    saun: 4,
+
+    bhadra: 5,
+    bhadau: 5,
+
+    ashwin: 6,
+    aswin: 6,
+    asoj: 6,
+
+    kartik: 7,
+    kartika: 7,
+
+    mangsir: 8,
+    marga: 8,
+    margashirsha: 8,
+
+    poush: 9,
+    paush: 9,
+    push: 9,
+
+    magh: 10,
+
+    falgun: 11,
+    phalgun: 11,
+
+    chaitra: 12,
+    chait: 12
+};
+
+export const NEPALI_MONTHS = {
+    "बैशाख": 1,
+    "वैशाख": 1,
+
+    "जेठ": 2,
+
+    "असार": 3,
+
+    "श्रावण": 4,
+    "साउन": 4,
+
+    "भदौ": 5,
+    "भद्र": 5,
+
+    "असोज": 6,
+    "आश्विन": 6,
+
+    "कार्तिक": 7,
+
+    "मंसिर": 8,
+    "मार्ग": 8,
+
+    "पुष": 9,
+    "पौष": 9,
+
+    "माघ": 10,
+
+    "फागुन": 11,
+    "फाल्गुण": 11,
+
+    "चैत": 12,
+    "चैत्र": 12
+};
+
+export const NEPALI_DIGITS = {
+    "०": "0",
+    "१": "1",
+    "२": "2",
+    "३": "3",
+    "४": "4",
+    "५": "5",
+    "६": "6",
+    "७": "7",
+    "८": "8",
+    "९": "9"
+};
+
+
+export const WEEKDAYS = [
+    "sunday",
+    "monday",
+    "tuesday",
+    "wednesday",
+    "thursday",
+    "friday",
+    "saturday"
+];
